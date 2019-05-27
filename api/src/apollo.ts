a45d3f800866e611475f8840d583d1b44fc7c977
Matt Thompson <matthew.thompson.a@gmail.com>
2019-05-27 13:48:28 -0400

    refactor with context

--- api/src/apollo.ts
+++ api/src/apollo.ts
@@ -1,33 +1,36 @@
 import { ApolloServer } from 'apollo-server';
+import { Context } from 'apollo-server-core';
+import { Request } from 'express';
+import { getCustomRepository } from 'typeorm';
+import User from './entities/User';
 import { schema } from './graphql/schema';
-// import { getCustomRepository } from 'typeorm';
-// import { UserRepository } from '../repositories/UserRepository';
-// import { Context } from 'apollo-server-core';
-// import User from '../entities/User';
-// import { Request } from 'express';
+import UserRepository from './repositories/User';
 
-/** Sets current user on context if a valid header was provided */
-// // const context = async (ctx: ContextParameters) => {
-// const context = async (ctx: Context<{ req: Request }>): Promise<ApolloContext> => {
-//   const repo = getCustomRepository(UserRepository);
-//   const authHeader = ctx.req.get('Authorization');
-//   let currentUser: User | undefined;
+const context = async (ctx: Context<{ req: Request }>): Promise<ApolloContext> => {
+  const repo = getCustomRepository(UserRepository);
+  const authHeader = ctx.req.get('Authorization');
+  let currentUser: User | undefined;
 
-//   if (authHeader) {
-//     currentUser = await repo.findFromAuthHeader(authHeader);
-//   }
+  if (authHeader) {
+    currentUser = await repo.getUserFromToken(authHeader);
+  }
 
-//   return { ...ctx, currentUser };
-// };
+  /**
+   * Ignore the the return value type here so TS doesn't force us to always check for currentUser
+   * in our resolvers. That check will be taken care of in the requireAuth directive resolver.
+   */
+  // @ts-ignore
+  return { ...ctx, currentUser };
+};
 
 export const server = () =>
   new ApolloServer({
     schema: schema,
     introspection: true,
-    playground: true
-    // context
+    playground: true,
+    context
   });
 
-// interface ApolloContext extends Context {
-//   currentUser: User | undefined;
-// }
+export interface ApolloContext extends Context {
+  currentUser: User;
+}
--- api/src/graphql/schema/index.ts
+++ api/src/graphql/schema/index.ts
@@ -1,8 +1,8 @@
 import { makeExecutableSchema } from 'apollo-server';
-import { HasRoleDirective } from './directives/hasRole';
-import { IsAuthenticatedDirective } from './directives/isAuthenticated';
-import resolvers from './schema/resolvers';
-import typeDefs from './schema/typeDefs';
+import { HasRoleDirective } from '../directives/hasRole';
+import { IsAuthenticatedDirective } from '../directives/isAuthenticated';
+import resolvers from './resolvers';
+import typeDefs from './typeDefs';
 
 export const schema = makeExecutableSchema({
   typeDefs,
